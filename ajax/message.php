d102bd4be399bb970fd2135cc6f35212ddb77517
AlexandraSpeshilova <spesilovasasa13@gmail.com>
2026-03-12 21:34:16 +0500

    шифровка комментов

--- ajax/message.php
+++ ajax/message.php
@@ -1,10 +1,44 @@
-<?
+<?php
     session_start();
-	include("../settings/connect_datebase.php");
+    include("../settings/connect_datebase.php");
+    function decryptAES($encryptedData, $key) {
+        $data = base64_decode($encryptedData);
+        if ($data === false || strlen($data) < 17) return false;
 
-    $IdUser = $_SESSION['user'];
-    $Message = $_POST["Message"];
-    $IdPost = $_POST["IdPost"];
+        $iv = substr($data, 0, 16);
+        $encrypted = substr($data, 16);
+        
+        $keyHash = md5($key);
+        $keyBytes = hex2bin($keyHash);
 
-    $mysqli->query("INSERT INTO `comments`(`IdUser`, `IdPost`, `Messages`) VALUES ({$IdUser}, {$IdPost}, '{$Message}');");
+        return openssl_decrypt(
+            $encrypted,
+            'aes-128-cbc',
+            $keyBytes,
+            OPENSSL_RAW_DATA,
+            $iv
+        );
+    }
+
+    $secretKey = "qazxswedcvfrtgbn";
+
+    $encryptedMessage = $_POST['Message'] ?? '';
+    $idPost = $_POST['IdPost'] ?? '';
+
+    $message = decryptAES($encryptedMessage, $secretKey);
+
+    if ($message && $idPost != '') {
+        $cleanMessage = $mysqli->real_escape_string($message);
+        $cleanIdPost = intval($idPost);
+
+        $sql = "INSERT INTO `comments` (`IdPost`, `Messages`) VALUES ('$cleanIdPost', '$cleanMessage')";
+        
+        if($mysqli->query($sql)) {
+            echo "Success";
+        } else {
+            echo "Error: " . $mysqli->error;
+        }
+    } else {
+        echo "Invalid data";
+    }
 ?>
--- index.php
+++ index.php
@@ -1,112 +1,125 @@
 <?php
-	session_start();
-	include("./settings/connect_datebase.php");
+    session_start();
+    include("./settings/connect_datebase.php");
 ?>
 <!DOCTYPE HTML>
 <html>
-	<head> 
-		<meta charset="utf-8">
-		<title> WEB-безопасность </title>
-		
-		<link rel="stylesheet" href="style.css">
-		<script src="https://code.jquery.com/jquery-1.8.3.js"></script>
-	</head>
-	<body>
-		<div class="top-menu">
-			<a class="button" href = "./login.php">Войти</a>
-		
-			<a href=#><img src = "img/logo1.png"/></a>
-			<div class="name">
-				<a href="index.php">
-					<div class="subname">БЗОПАСНОСТЬ  ВЕБ-ПРИЛОЖЕНИЙ</div>
-					Пермский авиационный техникум им. А. Д. Швецова
-				</a>
-			</div>
-			
-			
-		</div>
-		<div class="space"> </div>
-		<div class="main">
-			<div class="content">
-				<div class="name">Новости:</div>
-				
-				<div>
-					<?php
-						$query_news = $mysqli->query("SELECT * FROM `news`;");
-						while($read_news = $query_news->fetch_assoc()) {
-							$QueryMessages = $mysqli->query("SELECT * FROM `comments` WHERE `IdPost` = {$read_news["id"]}");
+    <head> 
+        <meta charset="utf-8">
+        <title> WEB-безопасность </title>
+        <link rel="stylesheet" href="style.css">
+        <script src="https://code.jquery.com/jquery-1.8.3.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js"></script>
+    </head>
+    <body>
+        <div class="top-menu">
+            <?php if (!isset($_SESSION['user'])): ?>
+                <a class="button" href="./login.php">Войти</a>
+            <?php else: ?>
+                <a class="button" href="./login.php">Выйти</a>
+            <?php endif; ?>
+        
+            <a href="#"><img src="img/logo1.png"/></a>
+            <div class="name">
+                <a href="index.php">
+                    <div class="subname">БЕЗОПАСНОСТЬ ВЕБ-ПРИЛОЖЕНИЙ</div>
+                    Пермский авиационный техникум им. А. Д. Швецова
+                </a>
+            </div>
+        </div>
+        <div class="space"> </div>
+        <div class="main">
+            <div class="content">
+                <div class="name">Новости:</div>
+                
+                <div>
+                    <?php
+                        $query_news = $mysqli->query("SELECT * FROM `news`;");
+                        while($read_news = $query_news->fetch_assoc()) {
+                            $QueryMessages = $mysqli->query("SELECT * FROM `comments` WHERE `IdPost` = {$read_news["id"]}");
 
-							echo '
-								<div class="specialty">
-									<div class = "slider">
-										<div class = "inner">
-											<div class="name">'.$read_news["title"].'</div>
-											<div class="description" style="overflow: auto;">
-												<img src = "'.$read_news["img"].'" style="width: 50px; box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.12), 0 1px 5px 0 rgba(0,0,0,.2); float: left; margin-right: 10px;">
-												'.$read_news["text"].'
-												
-											</div>
-											<div class="messages">
-												';
-												while($ReadMessages = $QueryMessages->fetch_assoc()) {
-													echo "<div>".$ReadMessages["Messages"]."</div>";
-												}
-											echo '</div>';
+                            echo '
+                                <div class="specialty">
+                                    <div class="slider">
+                                        <div class="inner">
+                                            <div class="name">'.$read_news["title"].'</div>
+                                            <div class="description" style="overflow: auto;">
+                                                <img src="'.$read_news["img"].'" style="width: 50px; float: left; margin-right: 10px;">
+                                                '.$read_news["text"].'
+                                            </div>
+                                            <div class="messages">';
+                                                while($ReadMessages = $QueryMessages->fetch_assoc()) {
+                                                    echo "<div>".htmlspecialchars($ReadMessages["Messages"])."</div>";
+                                                }
+                                            echo '</div>';
 
-											
-											if (isset($_SESSION['user'])) {
-												echo 
-													'<div class="messages" id="'.$read_news["id"].'">
-														<input type="text">
-														<div class="button" style="float: right; margin-top: 0px; margin-right: 0px;" onclick="SendMessage(this)">Отправить</div>
-													</div>';
-											}
-											
-										echo 
-										'</div>
-									</div>
-								</div>
-							';
-						}
-					?>
-					<div class="footer">
-						© КГАПОУ "Авиатехникум", 2020
-						<a href=#>Конфиденциальность</a>
-						<a href=#>Условия</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</body>
-	<script>
-		function SendMessage(sender) {
-			let Message = sender.parentElement.children[0].value;
-			let IdPost = sender.parentElement.id;
-			if(Message == "") return;
+                                            if (isset($_SESSION['user'])) {
+                                                echo 
+                                                    '<div class="messages" id="'.$read_news["id"].'">
+                                                        <input type="text" style="width: 80%;">
+                                                        <div class="button" style="float: right; margin-top: 0px;" onclick="SendMessage(this)">Отправить</div>
+                                                    </div>';
+                                            }
+                                            
+                                        echo '</div>
+                                    </div>
+                                </div>';
+                        }
+                    ?>
+                    <div class="footer">
+                        © КГАПОУ "Авиатехникум", 2020
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
 
-			var Data = new FormData();
-			Data.append("Message", Message);
-			Data.append("IdPost", IdPost);
-			
-			$.ajax({
-					url         : 'ajax/message.php',
-					type        : 'POST',
-					data        : Data,
-					cache       : false,
-					dataType    : 'html',
-					processData : false,
-					contentType : false, 
-					success: function (_data) {
-						console.log(_data);
-						sender.parentElement.children[0].value = "";
-						sender.parentElement.parentElement.children[2].innerHTML += "<div>" + Message + "</div>";
+    <script>
+        const secretKey = "qazxswedcvfrtgbn";
 
-					},
-					// функция ошибки
-					error: function( ){
-						console.log('Системная ошибка!');
-					}
-				});
-		}
-	</script>
+        function encryptAES(data, key) {
+            var keyHash = CryptoJS.MD5(key);
+            var keyBytes = CryptoJS.enc.Hex.parse(keyHash.toString());
+            var iv = CryptoJS.lib.WordArray.random(16);
+            var encrypted = CryptoJS.AES.encrypt(data, keyBytes, {
+                iv: iv,
+                mode: CryptoJS.mode.CBC,
+                padding: CryptoJS.pad.Pkcs7
+            });
+            var combined = iv.concat(encrypted.ciphertext);
+            return CryptoJS.enc.Base64.stringify(combined);
+        }
+
+        function SendMessage(sender) {
+            let inputField = sender.parentElement.children[0];
+            let Message = inputField.value;
+            let IdPost = sender.parentElement.id;
+            
+            if(Message == "") return;
+
+            let encryptedMessage = encryptAES(Message, secretKey);
+
+            var Data = new FormData();
+            Data.append("Message", encryptedMessage);
+            Data.append("IdPost", IdPost);
+            
+            $.ajax({
+                url         : 'ajax/message.php',
+                type        : 'POST',
+                data        : Data,
+                cache       : false,
+                processData : false,
+                contentType : false, 
+                success: function (_data) {
+                    console.log("Сервер ответил:", _data);
+                    inputField.value = "";
+                    let chatBox = sender.parentElement.parentElement.getElementsByClassName("messages")[0];
+                    chatBox.innerHTML += "<div>" + Message + "</div>";
+                },
+                error: function() {
+                    console.log('Системная ошибка!');
+                }
+            });
+        }
+    </script>
 </html>
