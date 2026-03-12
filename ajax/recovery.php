fd11570ff2fcef6a93b59e055ab5c8cce92356ae
AlexandraSpeshilova <spesilovasasa13@gmail.com>
2026-03-12 21:21:54 +0500

    шифровка восстановления пароля

--- ajax/recovery.php
+++ ajax/recovery.php
@@ -1,46 +1,51 @@
 <?php
-	session_start();
-	include("../settings/connect_datebase.php");
-	
-	$login = $_POST['login'];
-	
-	// ищем пользователя
-	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."';");
-	
-	$id = -1;
-	if($user_read = $query_user->fetch_row()) {
-		// создаём новый пароль
-		$id = $user_read[0];
-	}
-	
-	function PasswordGeneration() {
-		// создаём пароль
-		$chars="qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP"; // матрица
-		$max=10; // количество
-		$size=StrLen($chars)-1; // Определяем количество символов в $chars
-		$password="";
-		
-		while($max--) {
-			$password.=$chars[rand(0,$size)];
-		}
-		
-		return $password;
-	}
-	
-	if($id != 0) {
-		//обновляем пароль
-		$password = PasswordGeneration();;
-		// проверяем не используется ли пароль 
-		$query_password = $mysqli->query("SELECT * FROM `users` WHERE `password`= '".md5($password)."';");
-		while($password_read = $query_password->fetch_row()) {
-			// создаём новый пароль
-			$password = PasswordGeneration();
-		}
-		// обновляем пароль
-		$mysqli->query("UPDATE `users` SET `password`='".md5($password)."' WHERE `login` = '".$login."'");
-		// отсылаем на почту
-		//mail($login, 'Безопасность web-приложений КГАПОУ "Авиатехникум"', "Ваш пароль был только что изменён. Новый пароль: ".$password);
-	}
-	
-	echo $id;
+    session_start();
+    include("../settings/connect_datebase.php");
+    
+    function decryptAES($encryptedData, $key) {
+        $data = base64_decode($encryptedData);
+        if ($data === false || strlen($data) < 17) return false;
+        $iv = substr($data, 0, 16);
+        $encrypted = substr($data, 16);
+        $keyBytes = hex2bin(md5($key));
+        return openssl_decrypt($encrypted, 'aes-128-cbc', $keyBytes, OPENSSL_RAW_DATA, $iv);
+    }
+
+    $secretKey = "qazxswedcvfrtgbn";
+    $login_encrypted = $_POST['login'] ?? '';
+    $login = decryptAES($login_encrypted, $secretKey);
+
+    if (!$login) {
+        die("-1");
+    }
+
+    $login = $mysqli->real_escape_string($login);
+    $query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."';");
+    
+    $id = -1;
+    if($user_read = $query_user->fetch_row()) {
+        $id = $user_read[0];
+    }
+    
+    function PasswordGeneration() {
+        $chars="qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP";
+        $max=10;
+        $size=strlen($chars)-1;
+        $password="";
+        while($max--) {
+            $password.=$chars[rand(0,$size)];
+        }
+        return $password;
+    }
+    
+    if($id != -1) {
+        $password = PasswordGeneration();
+        $hashedPassword = md5($password);
+        
+        $mysqli->query("UPDATE `users` SET `password`='".$hashedPassword."' WHERE `login` = '".$login."'");
+        
+        echo $id;
+    } else {
+        echo "-1";
+    }
 ?>
